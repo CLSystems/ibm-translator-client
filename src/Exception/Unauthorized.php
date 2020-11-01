20bf7df9c18d8e78ba2d3005b17bfced9cb7d3e2
Jeroen Guyt <jeroen@clsystems.nl>
2020-11-01 07:50:00 +0100

    first commit

---
+++ src/Exception/Unauthorized.php
@@ -0,0 +1,11 @@
+<?php
+namespace CLSystems\IBMWatson\Translator\Exception;
+
+/**
+ * This is usually thrown when the API client cannot authorize against the API.
+ * Verify the credentials being supplied, and that the server is allowed to contact the API endpoint.
+ */
+class Unauthorized extends Transport
+{
+
+}
---
+++ src/Translator/Client.php
@@ -0,0 +1,105 @@
+<?php
+namespace CLSystems\IBMWatson\Translator\Translator;
+
+use CLSystems\IBMWatson\Translator\Api\Transport as ApiTransport;
+use CLSystems\IBMWatson\Translator\Exception\UnexpectedAPIResponse;
+
+/**
+ * This client encapsulates the translation domain (business logic).
+ */
+class Client implements ServiceInterface
+{
+    /**
+     * @var ApiTransport
+     */
+    protected $apiTransport;
+
+    public function __construct(ApiTransport $apiTransport)
+    {
+        $this->apiTransport = $apiTransport;
+    }
+
+	/**
+	 * Returns the 2 char language code.
+	 *
+	 * @param string $text
+	 * @return string
+	 */
+    public function identifyLanguage(string $text)
+    {
+        $response = $this->sendApiRequest('POST', 'identify', $text);
+        if (empty($response->languages)) {
+            $this->handleUnexpectedApiResponse('language identification');
+        }
+
+        return $response->languages[0]->language;
+    }
+
+    /**
+     * Interact with our bare API client to send a request.
+     * @param string $httpMethod
+     * @param string $apiUri
+     * @param string $requestBody
+     * @param bool $jsonRequestBodyFlag
+     * @return mixed
+     */
+    protected function sendApiRequest(
+        $httpMethod,
+        $apiUri,
+        $requestBody,
+        $jsonRequestBodyFlag = false
+    ) {
+        if ($jsonRequestBodyFlag) {
+            return $this->apiTransport->sendSynchronousJsonApiRequest(
+                $httpMethod,
+                $apiUri,
+                $requestBody
+            );
+        }
+
+        return $this->apiTransport->sendSynchronousPlainTextApiRequest(
+            $httpMethod,
+            $apiUri,
+            $requestBody
+        );
+    }
+
+	/**
+	 * Simple translation, specify text and target language.
+	 * This makes first a call to identify the source language.
+	 * Simple interface, but expensive since it requires 2 API calls
+	 *
+	 * @param string $text
+	 * @param string $targetLanguageCode 2 character language code.
+	 * @return string
+	 */
+    public function simpleTranslate(string $text, string $targetLanguageCode)
+    {
+        $translateRequest = new \stdClass();
+        $translateRequest->source = $this->identifyLanguage($text);
+        $translateRequest->target = $targetLanguageCode;
+        $translateRequest->text = [$text];
+
+        $response = $this->sendApiRequest(
+            'POST',
+            'translate',
+            json_encode($translateRequest),
+            true
+        );
+        if (empty($response->translations)) {
+            $this->handleUnexpectedApiResponse('translation');
+        }
+
+        return $response->translations[0]->translation;
+    }
+
+    /**
+     * This gets called when the API response was unexpected.
+     * @param string $failedActivity
+     * @throws UnexpectedAPIResponse
+     */
+    protected function handleUnexpectedApiResponse($failedActivity)
+    {
+        throw new UnexpectedAPIResponse("Unexpected API response for {$failedActivity}.");
+    }
+}
